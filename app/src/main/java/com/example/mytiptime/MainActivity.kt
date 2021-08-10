c1674031cbf9281df958674f59194c03b7c915e8
coretx09 <ngampiosauvet@gmail.com>
2021-08-10 14:04:42 +0300

    good job

--- app/src/main/java/com/example/mytiptime/MainActivity.kt
+++ app/src/main/java/com/example/mytiptime/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mytiptime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.mytiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,10 +35,15 @@ class MainActivity : AppCompatActivity() {
         var tip = tipPercentage * cost
 
         // SWITCH isChecked-- attribute (see if the switch is "ON")
-        val roundUp = binding.rounded.isChecked
+        val roundUp = binding.rounded.isChecked // TRUE OR FALSE
         if (roundUp) {
-            tip = kotlin.math.ceil(tip)
+            tip = kotlin.math.ceil(tip) // POUR ARRONDIR
         }
+
+        // FORMAT TIP
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
 
 
     }
